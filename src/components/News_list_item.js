06064f20f36bcb898adae283149b4f49018b0d2c
Junior Dugue <jrdugue@gmail.com>
2019-04-18 13:34:22 -0400

    destructed item inside of news list item component and resolved bugs

--- src/components/News_list_item.js
+++ src/components/News_list_item.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const NewsItem = props => {
+const NewsItem = ({ item }) => {
   return (
-    <div key={props.item.id}>
-      <h3>{props.item.title}</h3>
-      <div>{props.item.feed}</div>
+    <div key={item.id}>
+      <h3>{item.title}</h3>
+      <div>{item.feed}</div>
     </div>
   );
 };
