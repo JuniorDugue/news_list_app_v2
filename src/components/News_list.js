795daf3c69c17d9f7556e9cb742fdaeb1d8dd69d
Junior Dugue <jrdugue@gmail.com>
2019-04-18 13:32:36 -0400

    refactored newslist component into newslistitem to be more dynamic

--- src/components/News_list.js
+++ src/components/News_list.js
@@ -1,13 +1,16 @@
 import React from "react";
+import NewsItem from "./News_list_item";
 
 const newsList = props => {
+  // for(i=0; i < Array.length; i++) {
+  //   //this is the same as map function we're running
+  // }
+  //
+  // props.news.map(function(){
+  //   // is the same as the arrow function we're passing
+  // })
   const items = props.news.map(item => {
-    return (
-      <div>
-        <h3>{item.title}</h3>
-        <div>{item.feed}</div>
-      </div>
-    );
+    return <NewsItem key={item.id} item={item} />;
   });
 
   return <div>{items}</div>;
